223ae0e3169e56073f391383b94915925426373e
sechangchoi <scchoi5@naver.com>
2022-05-06 13:26:05 +0900

    1부터 20까지 출력하고 3, 5, 15의 배수일 때 각각 맞는 문자열 출력

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-# 과제: 1부터 10까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
+# 과제: 1부터 20까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
 # 나머지 모든 경우는 숫자 그대로 출력
-for i in range(1, 11):
-	if i%3 == 0:
-		print('year')
-	else:
-		print(i)
+# 15의 배수일 때, yeardream
+# 5의 배수일 때, dream 출력
+for i in range(1, 21):
+		if i%15 == 0:
+			print('yeardream')
+		elif i%5 == 0:
+			print('dream')
+		elif i%3 == 0:
+			print('year')
+		else:
+			print(i)
 print('hello')
